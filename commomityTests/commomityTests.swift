2b85ff506ab6f4a1448202f209b150c14814988e
DrewHengehold <drew.hengehold@modsquad.com>
2026-03-20 14:18:34 -0700

    Initial Commit

---
+++ commomityTests/commomityTests.swift
@@ -0,0 +1,17 @@
+//
+//  commomityTests.swift
+//  commomityTests
+//
+//  Created by Drew Hengehold on 3/20/26.
+//
+
+import Testing
+@testable import commomity
+
+struct commomityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
